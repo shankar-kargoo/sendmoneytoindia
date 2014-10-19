38ffe0e6b411ed1e511685220602ca8113619f74
Shankar Narayan <shankar@kargoo.com>
2014-10-19 02:02:39 -0700

    Fixed date bug

--- app/controllers/forexs_controller.rb
+++ app/controllers/forexs_controller.rb
@@ -10,14 +10,14 @@ class ForexsController < ApplicationController
 		@sbi = FxSbi.last.rate
 		@bloomberg = FxBloomberg.last.rate
 
-		@wu_time = FxWu.last.created_at.to_formatted_s(:long).in_time_zone('America/Los_Angeles')
-		@rti_time = FxRti.last.created_at.to_formatted_s(:long).in_time_zone('America/Los_Angeles')
-		@google_time = FxGoogle.last.created_at.to_formatted_s(:long).in_time_zone('America/Los_Angeles')
-		@xoom_time = FxXoom.last.created_at.to_formatted_s(:long).in_time_zone('America/Los_Angeles')
-		@icici_time = FxIcici.last.created_at.to_formatted_s(:long).in_time_zone('America/Los_Angeles')
-		@hdfc_time = FxHdfc.last.created_at.to_formatted_s(:long).in_time_zone('America/Los_Angeles')
-		@sbi_time = FxSbi.last.created_at.to_formatted_s(:long).in_time_zone('America/Los_Angeles')
-		@bloomberg_time = FxBloomberg.last.created_at.to_formatted_s(:long).in_time_zone('America/Los_Angeles')
+		@wu_time = FxWu.last.created_at.to_formatted_s(:long)
+		@rti_time = FxRti.last.created_at.to_formatted_s(:long)
+		@google_time = FxGoogle.last.created_at.to_formatted_s(:long)
+		@xoom_time = FxXoom.last.created_at.to_formatted_s(:long)
+		@icici_time = FxIcici.last.created_at.to_formatted_s(:long)
+		@hdfc_time = FxHdfc.last.created_at.to_formatted_s(:long)
+		@sbi_time = FxSbi.last.created_at.to_formatted_s(:long)
+		@bloomberg_time = FxBloomberg.last.created_at.to_formatted_s(:long)
 
 		@user = User.new
 	
